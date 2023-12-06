2a90170a66168cdbd08523f28f8494bfa6203a26
Rajat Dua <rajatdua95@gmail.com>
2023-12-06 14:57:20 +0100

    feat(sentiment): get tweets

--- pages/multi-coord-view/SentimentContainer.tsx
+++ pages/multi-coord-view/SentimentContainer.tsx
@@ -1,10 +1,20 @@
 import ParentSize from '@visx/responsive/lib/components/ParentSize';
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, MouseEvent} from "react";
 import ColumnChart from "../../components/ColumnChart/ColumnChart";
+import Popup from "../../components/Popup/Popup";
 import Select from "../../components/Select/Select";
+import Sidebar from "../../components/Sidebar/Sidebar";
 import Spinner from "../../components/Spinner/Spinner";
+import Tweet from "../../components/Tweet/Tweet";
 import {INIT_SENTIMENT} from "../../constants";
-import {ICommonChartProps, IFetchSentimentData, IFetchSentimentReq, SentimentItem} from "../../types";
+import {
+  ICommonChartProps,
+  IFetchSentimentData,
+  IFetchSentimentReq,
+  IFetchTweetData,
+  IFetchTweetReq,
+  SentimentItem
+} from "../../types";
 import {createDateQuery} from "../../utils/client";
 
 function convertToSentimentArray(sentimentCounts: IFetchSentimentData, selectedScale: string): SentimentItem[] {
@@ -29,9 +39,13 @@ const scaleOptions = [{value: 'name', label: 'Sort by Name'}, {value: 'value', l
 
 const SentimentContainer: React.FC<ICommonChartProps> = ({ date, refreshCount, setRefreshing, setTotalTweets }) => {
   const [sentimentData, setSentimentData] = useState<IFetchSentimentData>(INIT_SENTIMENT);
+  const [isLoadingTweets, setLoadingTweets] = useState(true);
   const [isLoading, setLoading] = useState(true);
+  const [fetchedTweets, setFetchedTweets] = useState<IFetchTweetData[]>([])
   const [selectedScale, setScale] = useState<'name' | 'value'>('value');
-
+  const [isMenuOpen, setMenu] = useState(false);
+  const [tweetsToView, setTweets] = useState<string[]>([]);
+  const [isSidebar, setSidebar] = useState(false);
 
   useEffect(() => {
     const fetchPollData = async () => {
@@ -58,6 +72,41 @@ const SentimentContainer: React.FC<ICommonChartProps> = ({ date, refreshCount, s
     else throw Error(`Wrong scale selected: ${selectedScale}`)
   };
 
+  const handleColumnClick = (_: MouseEvent, selectedEntry: SentimentItem) => {
+    setMenu(true);
+    setTweets(selectedEntry.value.tweets);
+  };
+
+  const options = [
+    {
+      label: 'View Tweets', clickEvent: async () => {
+        setMenu(false);
+        setSidebar(true);
+        const fetchedData = await (await fetch('/api/tweets', {
+          method: 'POST',
+          body: JSON.stringify({ids: tweetsToView})
+        })).json() as IFetchTweetReq;
+        setFetchedTweets(fetchedData.data ?? []);
+        setLoadingTweets(false);
+      }
+    },
+    {
+      label: 'Close', clickEvent: () => {
+        setMenu(false);
+      }
+    },
+  ];
+
+  const sidebarChildren = () => {
+    return fetchedTweets.map((tweet, index) => {
+      return (
+        <li key={index}>
+          <Tweet tweetHTML={tweet.content}/>
+        </li>
+      );
+    })
+  };
+
   // TODO: Give ability to select bars and show pop-up menu for export
   if (isLoading) return <div className="flex flex-col justify-center" style={{ height: '420px' }}><Spinner /></div>
   else {
@@ -66,7 +115,20 @@ const SentimentContainer: React.FC<ICommonChartProps> = ({ date, refreshCount, s
       <div className="flex justify-end">
         <Select handleChange={handleChange} options={scaleOptions} preSelected={selectedScale}/>
       </div>
-      <ParentSize>{({ width }: { width: number }) => <ColumnChart width={width} data={convertToSentimentArray(sentimentData, selectedScale)} onChartRender={handleChartRender} />}</ParentSize>
+      <ParentSize>{({ width }: { width: number }) => <ColumnChart handleColumnClick={handleColumnClick} width={width} data={convertToSentimentArray(sentimentData, selectedScale)} onChartRender={handleChartRender} />}</ParentSize>
+      {isMenuOpen && (
+        <Popup options={options}/>
+      )}
+      {isSidebar && (
+        <Sidebar isSidebar={isSidebar} onClose={() => {
+          setSidebar(false);
+          setLoadingTweets(true);
+          setTweets([]);
+          setFetchedTweets([]);
+        }} title={`Tweets for selected data points ${isLoadingTweets ? '...' : `(${fetchedTweets.length})`}`}>
+          {isLoadingTweets ? <div className='pt-48'><Spinner/></div> : sidebarChildren()}
+        </Sidebar>
+      )}
       </div>
     );
   }
